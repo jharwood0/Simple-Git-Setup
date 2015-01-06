65b5b6126ba45df69fb17f2616b032e4680a4028
Josh Harwood <bocharwood@gmail.com>
2015-01-06 23:47:37 +0000

    added checks and added efficiency

--- setup.sh
+++ setup.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
-echo "##~Please install git first~##"
+out=$(git)
+if [ $? == 0 ]
+then
+	echo "Please install git first!"
+	exit
+fi
 echo "Please enter a name:"
 read name
 echo "please enter an email address:"
 read email
-user_name="git config --global user.name \"$name\" "
-email_address="git config --global user.email \"$email\" "
-eval $user_name
-eval $email_address
+out=$(git config --global user.name "$name")
+out=$(git config --global user.email "$email")
 echo "Would you like to cache your username and password?(y/n)"
 read yn
 if [ "$yn" = "y" ]
 then
-	cache="git config --global credential.helper 'cache --timeout=3600'"
-	eval $cache
+	out=$(git config --global credential.helper 'cache --timeout=3600')
 fi
-echo "Would you like to set your default editor?"
+echo "Would you like to set your default editor?(y/n)"
 read yn
 if [ "$yn" = "y" ]
+then
 	echo "Please enter your default editor:(emacs,vim,nano, etc..)"
 	read editor
-	edit="git config --global core.editor $editor"
-	eval $edit
+	out=$(git config --global core.editor $editor)
+
+
 fi
 echo "All Done!"
-git config -list
+git config --list
